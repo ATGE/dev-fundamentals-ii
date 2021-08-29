ff2a0f1c20bb63098bbd5882bd365d35532e2531
ATGE <german.eduardo.qm@gmail.com>
2021-08-29 19:26:32 -0400

    Add task 2

---
+++ tasks/task2/api/driver.py
@@ -0,0 +1,25 @@
+""" Drivers data """
+
+drivers = [
+    {
+        'id': 0,
+        'name': 'Testerson',
+        'last_name': 'Testerson',
+        'license_number': '123',
+        'trunck_id': '0'
+    },
+    {
+        'id': 1,
+        'name': 'Jon',
+        'last_name': 'Wick',
+        'license_number': '456',
+        'trunck_id': '1'
+    },
+    {
+        'id': 2,
+        'name': 'Neo',
+        'last_name': 'One',
+        'license_number': '789',
+        'trunck_id': '2'
+    }
+]
