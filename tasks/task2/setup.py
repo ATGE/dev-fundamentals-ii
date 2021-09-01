32322d96ecc0cd84bef986c92158e6fd9a4bb2bf
ATGE <german.eduardo.qm@gmail.com>
2021-08-31 22:39:29 -0400

    Add files from session 4

---
+++ tasks/task2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="truck_deliver_atge",
+    version="0.0.1",
+    author="atge",
+    description="an application to manage trucks",
+    requires_dist=['flask']
+)
