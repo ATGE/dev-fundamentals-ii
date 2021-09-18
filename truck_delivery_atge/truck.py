65a07ec94e8dd754e5757ee47b3d4bf777c26ef2
ATGE <german.eduardo.qm@gmail.com>
2021-09-18 12:41:02 -0400

    update class Truck

--- truck_delivery_atge/truck.py
+++ truck_delivery_atge/truck.py
@@ -1,11 +1,18 @@
 
-class Truck:
-    def __init__(self, name):
-        self.name = name
+from truck_delivery_atge.vehicle import Vehicle
+
+
+class Truck(Vehicle):
+    def __init__(self,type, name,model ,brand_manufactor,driver=None):
+        super(Truck, self).__init__(type, name)
+        self.model = model
+        self.brand_manufactor = brand_manufactor
+        self.driver = driver
 
     def to_dict(self):
-        return {}
+        dict_init = self.__dict__
+        return dict_init
 
     def get_driver(self):
-        pass
+        return self.driver
 
