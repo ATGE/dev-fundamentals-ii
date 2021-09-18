227e699e049b3b23d2e6a069586956f115951221
ATGE <german.eduardo.qm@gmail.com>
2021-09-18 12:32:21 -0400

    update driver class

--- truck_delivery_atge/driver.py
+++ truck_delivery_atge/driver.py
@@ -1,5 +1,15 @@
+from truck_delivery_atge.person import Person
 
 
-class Driver:
-    def __init__(self, name):
-        self.name = name
+class Driver(Person):
+    """ Class representing a Driver """
+    def __init__(self, name, email, cellphone, address, license_number, license_country):
+        super(Driver, self).__init__(name, email, cellphone, address)
+        self.license_number = license_number
+        self.license_country = license_country
+
+    def to_dict(self):
+        dict_init = self.__dict__
+        return dict_init
+
+
