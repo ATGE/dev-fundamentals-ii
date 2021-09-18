eae8a8927f2ef71c03f9b4bf948b14e601d8bcbe
ATGE <german.eduardo.qm@gmail.com>
2021-09-18 18:50:03 -0400

    add DBConnectorRedis

--- tests/funcional/test_db_connector.py
+++ tests/funcional/test_db_connector.py
@@ -1,11 +1,11 @@
 import pytest
-from truck_delivery_atge.db_connector import DBConnector
+from truck_delivery_atge.db_connector_redis import DBConnectorRedis
 
 def test_save():
     id_test = "123"
     object_to_save = {"123": {"a": "a"}}
 
-    db_connector = DBConnector.instance()
+    db_connector = DBConnectorRedis.instance()
     result_save = db_connector.save(id_test, object_to_save)
     assert result_save
 
@@ -14,7 +14,7 @@ def test_get_by_id():
     id_test = "123"
     object_to_save = {"123": {"a":"a"}}
 
-    db_connector = DBConnector.instance()
+    db_connector = DBConnectorRedis.instance()
     db_connector.save(id_test, object_to_save)
 
     result = db_connector.get_by_id(id_test)
@@ -24,7 +24,7 @@ def test_get_all():
     id_test = "123"
     object_to_save = {"123": {"a":"a"}}
 
-    db_connector = DBConnector.instance()
+    db_connector = DBConnectorRedis.instance()
     db_connector.save(id_test, object_to_save)
 
     result = db_connector.get_all()
@@ -34,7 +34,7 @@ def test_delete():
     id_test = "123"
     object_to_save = {"123": {"a":"a"}}
 
-    db_connector = DBConnector.instance()
+    db_connector = DBConnectorRedis.instance()
     db_connector.save(id_test, object_to_save)
 
     result = db_connector.delete_by_id(id_test)
--- truck_delivery_atge/db_connector.py
+++ truck_delivery_atge/db_connector.py
@@ -1,62 +1,31 @@
-import redis
-import json
-import threading
-class DBConnector:
-
-    _instance = None
-    _lock = threading.Lock()
-
-    _instance = None
+from abc import ABCMeta, abstractmethod
+class DBConnector(metaclass=ABCMeta):
 
     def __init__(self):
         raise RuntimeError('Call instance() instead')
 
     @classmethod
+    @abstractmethod
     def instance(cls):
-        if cls._instance is None:
-            cls._instance = cls.__new__(cls)
-            cls.connection = redis.Redis(host="localhost",
-                                          port=6379)
-        return cls._instance
-
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, *exc):
-        # close database connection
         pass
 
+    @abstractmethod
     def save(self, id_save, object_to_save):
-        encode_data = json.dumps(object_to_save)
-        result_save = self.connection.set(id_save, encode_data)
-        return result_save
-
+        pass
+    
+    @abstractmethod
     def get_by_id(self, id):
-        result_get = self.connection.get(id)
-        decode_data = json.loads(result_get)
-        return decode_data
+        pass
 
+    @abstractmethod
     def get_all(self):
-        list_objs = []
-        list_ids = [id for id in self.connection.scan_iter(count=20)]
-        if len(list_ids) > 0:
-            list_objs = self.connection.mget(list_ids)
-            list_objs = [json.loads(obj) for obj in list_objs]
-        return list_objs
-
+        pass
+    
+    @abstractmethod
     def get_by_pattern(self, pattern):
-        pattern = f"{pattern}*"
-        list_objs = []
-        list_ids = [id for id
-                    in self.connection.scan_iter(match=pattern,
-                                                 count=20)
-                    ]
-        if len(list_ids) > 0:
-            list_objs = self.connection.mget(list_ids)
-            list_objs = [json.loads(obj) for obj in list_objs]
-        return list_objs
+        pass
 
+    @abstractmethod
     def delete_by_id(self, id):
-        return self.connection.delete(id)
+        pass
 
