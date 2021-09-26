a58926bf48adfeae1a7bc3605ecece644a63ba64
ATGE <german.eduardo.qm@gmail.com>
2021-09-25 22:19:18 -0400

    add driver manager

--- truck_delivery_atge/driver.py
+++ truck_delivery_atge/driver.py
@@ -3,8 +3,8 @@ from truck_delivery_atge.person import Person
 
 class Driver(Person):
     """ Class representing a Driver """
-    def __init__(self, name, email, cellphone, address, license_number, license_country):
-        super(Driver, self).__init__(name, email, cellphone, address)
+    def __init__(self,ci, name, email='', cellphone='', address = '', license_number= '', license_country= ''):
+        super(Driver, self).__init__(ci, name, email, cellphone, address)
         self.license_number = license_number
         self.license_country = license_country
 
